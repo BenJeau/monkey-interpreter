d4f65c6e015d17703ab88db558f76a809df6e2d9
Benoit Jeaurond <benoit@jeaurond.dev>
2024-09-07 16:47:04 -0400

    feat: use the new wasm functions in UI

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,17 +1,25 @@
-import { parseProgram } from "monkey_interpreter";
+import { evalProgram } from "monkey_interpreter";
 import { useEffect, useState } from "react";
 
 function App() {
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
+  const [time, setTime] = useState<number | undefined>(undefined);
 
   useEffect(() => {
-    setOutput(parseProgram(input));
+    const start = performance.now();
+    const result = evalProgram(input);
+    const end = performance.now();
+
+    setTime(end - start);
+
+    setOutput(result);
   }, [input]);
 
   return (
     <div className="App">
       <h1>Monkey Interpreter</h1>
+      <p>Took {time} ms to evaluate</p>
       <textarea value={input} onChange={(e) => setInput(e.target.value)} />
       <pre>{output}</pre>
     </div>
