ae23e6dbac9609a5cbdbf783d8c35c75a67d3207
Benoit Jeaurond <benoit@jeaurond.dev>
2024-09-13 19:42:19 -0400

    feat: serialize expression with serde override for hash literal

--- src/ast.rs
+++ src/ast.rs
@@ -77,7 +77,34 @@ pub enum Expression {
         left: Box<Expression>,
         index: Box<Expression>,
     },
+    #[cfg_attr(target_family = "wasm", serde(with = "btreemap_as_list"))]
     HashLiteral(BTreeMap<Expression, Expression>),
+}
+
+#[cfg(target_family = "wasm")]
+mod btreemap_as_list {
+    use serde::ser::SerializeSeq;
+    use serde::{Serialize, Serializer};
+    use std::collections::BTreeMap;
+
+    #[derive(Serialize)]
+    struct Entry<K: Serialize, V: Serialize> {
+        key: K,
+        value: V,
+    }
+
+    pub fn serialize<S, K, V>(map: &BTreeMap<K, V>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+        K: Serialize + Ord,
+        V: Serialize,
+    {
+        let mut seq = serializer.serialize_seq(Some(map.len()))?;
+        for (key, value) in map {
+            seq.serialize_element(&Entry { key, value })?;
+        }
+        seq.end()
+    }
 }
 
 impl std::fmt::Display for Expression {
