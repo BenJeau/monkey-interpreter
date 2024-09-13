c205554bc580660839a7436991bced47f89700cd
Benoit Jeaurond <benoit@jeaurond.dev>
2024-09-13 13:25:44 -0400

    feat: improve initial page load experience

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -6,7 +6,32 @@ import App from "@/App";
 import "@/lib/monaco";
 import "@/index.css";
 
-createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+const loadingContainer = document.getElementById("loading-container");
+const loadingContent = document.getElementById("loading-content");
+
+if (loadingContent) {
+  loadingContent.classList.add("opacity-100");
+}
+
+if (loadingContainer) {
+  setTimeout(() => {
+    loadingContainer.style.background = "#00000000";
+    setTimeout(() => {
+      loadingContainer.style.opacity = "0";
+      setTimeout(() => {
+        loadingContainer.remove();
+      }, 100);
+    }, 200);
+  }, 100);
+}
+
+createRoot(root).render(
   <StrictMode>
     <App />
     <Toaster />
