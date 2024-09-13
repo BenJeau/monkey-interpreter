ba2585230d1be2fd2ba50d4ef7b39eeed493e12d
Benoit Jeaurond <benoit@jeaurond.dev>
2024-09-13 17:38:30 -0400

    feat: update monaco language def for strings and []

--- ui/src/lib/monaco-monkey-lang-definition.ts
+++ ui/src/lib/monaco-monkey-lang-definition.ts
@@ -9,12 +9,14 @@ export const conf: languages.LanguageConfiguration = {
   autoClosingPairs: [
     { open: "{", close: "}" },
     { open: "(", close: ")" },
-    // { open: '"', close: '"' },
+    { open: "[", close: "]" },
+    { open: '"', close: '"' },
   ],
   surroundingPairs: [
     { open: "(", close: ")" },
     { open: "{", close: "}" },
-    // { open: '"', close: '"' },
+    { open: "[", close: "]" },
+    { open: '"', close: '"' },
   ],
 };
 
@@ -76,13 +78,20 @@ export const language = <languages.IMonarchLanguage>{
       [/[;,.]/, "delimiter"],
 
       // strings
-      // [/"([^"\\]|\\.)*$/, "string.invalid"], // non-teminated string
-      // [/"/, { token: "string.quote", bracket: "@open", next: "@string" }],
+      [/"([^"\\]|\\.)*$/, "string.invalid"], // non-teminated string
+      [/"/, { token: "string.quote", bracket: "@open", next: "@string" }],
 
       // characters
       [/'[^\\']'/, "string"],
       [/(')(@escapes)(')/, ["string", "string.escape", "string"]],
       [/'/, "string.invalid"],
+    ],
+
+    string: [
+      [/[^\\"]+/, "string"],
+      [/@escapes/, "string.escape"],
+      [/\\./, "string.escape.invalid"],
+      [/"/, { token: "string.quote", bracket: "@close", next: "@pop" }],
     ],
 
     whitespace: [[/[ \t\r\n]+/, "white"]],
